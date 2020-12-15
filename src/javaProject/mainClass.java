4caee69aae70e0e41f294262b72b72853593f615
tobita <tobita@192.168.1.10>
2020-12-15 13:00:50 +0900

    九九計算表を作成してみた。

--- src/javaProject/mainClass.java
+++ src/javaProject/mainClass.java
@@ -1,10 +1,18 @@
 package javaProject;
 
 public class mainClass {
+	String name = "てすと";
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("あいうえおか");
+		System.out.println("九九の表");
+
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 9; j++) {
+				System.out.print(i * j + " ");
+			}
+			System.out.println();
+		}
 	}
 
 }
